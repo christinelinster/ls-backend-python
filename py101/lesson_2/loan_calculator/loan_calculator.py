7e96758414faf6c5d1fec31f8cabd3e072e4bb51
Christine Lin <lin.christine19@gmail.com>
2025-03-25 11:16:16 -0400

    refactor code based on pylint

--- py101/lesson_2/loan_calculator/loan_calculator.py
+++ py101/lesson_2/loan_calculator/loan_calculator.py
@@ -1,33 +1,31 @@
 import json
 
 # Import JSON messages
-with open('messages.json', 'r') as file: 
+with open('messages.json', 'r') as file:
     MESSAGES = json.load(file)
-    
-# Welcome message
+
 print(MESSAGES['welcome'])
 
 # Validate inputs
-def is_invalid(input):
-    if input.casefold() == 'NaN'.casefold():
+def is_invalid(user_input):
+    if user_input.casefold() == 'NaN'.casefold():
         return True
     try:
-        float(input)
+        float(user_input)
     except ValueError:
         return True
     return False
 
-def is_zero(input):
-    if not float(input):
+def is_zero(user_input):
+    if not float(user_input):
         return True
     return False
-
 
 # Helper Functions
 def prompt(message):
     return(f'>>> {message}')
 
-def get_loan_amount(): 
+def get_loan_amount():
     loan_amount = input(prompt(MESSAGES['loan_amount']))
     while is_invalid(loan_amount):
         print(prompt('Please provide a valid number.'))
@@ -36,26 +34,31 @@ def get_loan_amount():
 
 def get_apr():
     apr = input(prompt(MESSAGES['apr']))
-    while is_invalid(apr) or is_zero(apr): 
-        print(prompt('Please provide a valid percentage (%) that is greater than 0.'))
+    while is_invalid(apr) or is_zero(apr):
+        print(prompt('Please provide a valid '
+                     'percentage (%) that is greater than 0.'))
         apr = input(prompt(MESSAGES['apr']))
-    
     return float(apr)
 
 def get_years():
     num_of_years = input(prompt(MESSAGES['num_of_years']))
     while is_invalid(num_of_years) or is_zero(num_of_years):
-        print(prompt('Please provide a valid number of years that is greater than 0.'))
+        print(prompt('Please provide a valid '
+                     'number of years that is greater than 0.'))
         num_of_years = input(prompt(MESSAGES['num_of_years']))
     return float(num_of_years)
 
 # Main Function
 def get_monthly_payment(amount, apr, years):
     monthly_interest_rate = (apr / 12) / 100
-    num_of_months = years * 12 
-    monthly_payment = amount * ((monthly_interest_rate) / (1 - (1 + monthly_interest_rate) ** (-num_of_months)))
+    num_of_months = years * 12
+    monthly_payment = amount * ((monthly_interest_rate) /
+                      (1 - (1 + monthly_interest_rate) ** (-num_of_months)))
     return monthly_payment
 
-# Calculate and output monthly payments to user 
-total_monthly_payments = get_monthly_payment(get_loan_amount(), get_apr(), get_years())
-print(prompt(f'Your total monthly payments are: ${total_monthly_payments:.2f}!'))
+# Calculate and output monthly payments to user
+total_monthly_payments = get_monthly_payment(get_loan_amount(),
+                                             get_apr(),
+                                             get_years())
+print(prompt('Your total monthly payments are: '
+             f'${total_monthly_payments:.2f}!'))
