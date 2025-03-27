7f111b1563c7b48e0a5365323b5b8c0561eaddd6
Christine Lin <lin.christine19@gmail.com>
2025-03-27 11:22:19 -0400

    enable another calculation with user prompt

--- py101/lesson_2/loan_calculator/loan_calculator.py
+++ py101/lesson_2/loan_calculator/loan_calculator.py
@@ -8,7 +8,8 @@ print(MESSAGES['welcome'])
 
 # Validate inputs
 def is_invalid(user_input):
-    if user_input.casefold() == 'nan'.casefold():
+    INVALID_INPUTS = {'nan', 'inf'}
+    if user_input.casefold() in INVALID_INPUTS:
         return True
     try:
         number = float(user_input)
@@ -20,28 +21,26 @@ def is_invalid(user_input):
 
 # Helper Functions
 def prompt(message):
-    return(f'>>> {message}')
+    return f'>>> {message}'
 
 def get_loan_amount():
     loan_amount = input(prompt(MESSAGES['loan_amount']))
     while is_invalid(loan_amount):
-        print(prompt('Please provide a valid number greater than 0.'))
+        print(prompt(MESSAGES['zero_error']))
         loan_amount = input(prompt(MESSAGES['loan_amount']))
     return float(loan_amount)
 
 def get_apr():
     apr = input(prompt(MESSAGES['apr']))
     while is_invalid(apr):
-        print(prompt('Please provide a valid '
-                     'percentage (%) that is greater than 0.'))
+        print(prompt(MESSAGES['zero_error']))
         apr = input(prompt(MESSAGES['apr']))
     return float(apr)
 
 def get_years():
     num_of_years = input(prompt(MESSAGES['num_of_years']))
     while is_invalid(num_of_years):
-        print(prompt('Please provide a valid '
-                     'number of years that is greater than 0.'))
+        print(prompt(MESSAGES['zero_error']))
         num_of_years = input(prompt(MESSAGES['num_of_years']))
     return float(num_of_years)
 
@@ -54,8 +53,18 @@ def get_monthly_payment(amount, apr, years):
     return monthly_payment
 
 # Calculate and output monthly payments to user
-total_monthly_payments = get_monthly_payment(get_loan_amount(),
-                                             get_apr(),
-                                             get_years())
-print(prompt('Your total monthly payments are: '
+while True: 
+    total_monthly_payments = get_monthly_payment(get_loan_amount(),
+                                                get_apr(),
+                                                get_years())
+    
+    print(prompt('Your total monthly payments are: '
              f'${total_monthly_payments:.2f}!'))
+    
+    another_calculation = input(prompt(MESSAGES['continue']))
+
+    if another_calculation.lower() == 'n':
+        break
+
+    
+    
