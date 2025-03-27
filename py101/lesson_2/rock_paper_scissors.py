cc80d1f2256dae332ad4ce2f4660e9b0e057ff77
Christine Lin <lin.christine19@gmail.com>
2025-03-27 15:59:39 -0400

    track best of 5 wins

--- py101/lesson_2/rock_paper_scissors.py
+++ py101/lesson_2/rock_paper_scissors.py
@@ -16,27 +16,48 @@ WINNING_CHOICES = {
     "lizard": ["paper", "spock"]
 }
 
+computer_wins = 0
+player_wins = 0
+winner = False
+
 # Helper Functions
 def prompt(message):
     print(f"==> {message}")
 
-def display_winner(player, computer):
+def get_point(player, computer):
+    global player_wins
+    global computer_wins
     prompt(f"You chose {player}, computer chose {computer}")
     
     if computer in WINNING_CHOICES[player]:
-        prompt("You win!")
+        player_wins += 1
+        prompt("One point for you!")
+
     elif player in WINNING_CHOICES[computer]:
-        prompt("Computer wins!")
+        computer_wins += 1
+        prompt("One point for the computer!")
     else:
         prompt("It's a tie!")
+    prompt(f"Player points: {player_wins} points")
+    prompt(f"Computer points: {computer_wins} points")
 
 def get_choice(choice):
+    choice = choice.lower()
     for name, abbr in VALID_CHOICES.items():
         if choice in abbr:
             return name
     return None
 
-while True:
+def display_winner(p_wins, c_wins):
+    global winner
+    if p_wins >= 5:
+        prompt("WINNER! WINNER! You win this round!")
+        winner = True
+    elif c_wins >= 5:
+        prompt("DEFEAT :( The computer wins!")
+        winner = True
+
+def main():
     prompt(f'Choose one: {", ".join(CHOICES)}')
     choice = get_choice(input())
 
@@ -45,17 +66,25 @@ while True:
         choice = get_choice(input())
 
     computer_choice = random.choice(CHOICES)
+    get_point(choice, computer_choice)
 
-    display_winner(choice, computer_choice)
+    if player_wins >= 5 or computer_wins >=5:
+        display_winner(player_wins, computer_wins)
 
-    while True:
-        prompt("Do you want to play again (y/n)?")
-        answer = input().lower()
 
-        if answer.startswith('n') or answer.startswith('y'):
-            break
-        else:
+while not winner or True:
+    main()
+
+    if winner:
+        while True:
+            prompt("Do you want to play again (y/n)?")
+            answer = input().lower()
+            if answer.startswith('n') or answer.startswith('y'):
+                break
             prompt("That's not a valid choice")
+        if answer[0] == 'n':
+            break
+        computer_wins = 0 
+        player_wins = 0 
+        winner = False
 
-    if answer[0] == 'n':
-        break
